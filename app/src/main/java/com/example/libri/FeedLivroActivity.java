061afdb98b5b31c3f109c2d1eaaa4e75bed14304
Felipe de Melo Fernandes <79909903+FelipeMel0@users.noreply.github.com>
2022-02-24 09:45:48 -0300

    Criando menu

--- app/src/main/java/com/example/libri/FeedLivroActivity.java
+++ app/src/main/java/com/example/libri/FeedLivroActivity.java
@@ -1,8 +1,13 @@
 package com.example.libri;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class FeedLivroActivity extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class FeedLivroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_livro);
+    }
+
+    /* Inflate de menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /* Ações do menu */
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        Log.d("MENUITEM-", String.valueOf(item.getItemId()));
+
+        switch (item.getItemId()){
+
+            case R.id.menu_cadastrar_livro:
+                startActivity(new Intent(this, CadastroLivroActivity.class));
+                break;
+
+            case R.id.menu_feed_livro:
+                startActivity(new Intent(this, FeedLivroActivity.class));
+                break;
+
+            case R.id.menu_sair:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
